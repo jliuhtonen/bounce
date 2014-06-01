2fced79a1babcdebc3ebad43553b8eb1374f2b8a
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2014-06-01 18:23:45 +0300

    use requestAnimationFrame instead of setTimeout for game loop

--- src/cljs_pong/core.cljs
+++ src/cljs_pong/core.cljs
@@ -5,21 +5,13 @@
 
 (enable-console-print!)
 
-(def fps 60)
-(def next-update-in-ms (/ 1000 fps))
-
-(declare game-loop)
-
-(defn- schedule [f t] (js/setTimeout f t))
-
-(defn- schedule-next [state]
-  (schedule #(game-loop state) next-update-in-ms))
+(defn- schedule [f] (js/requestAnimationFrame f))
 
 (defn- game-loop [state]
   (let [new-state (logic/update-state state (keyhandler/get-player-actions))]
     (draw/draw-game new-state)
     (if (:running new-state)
-      (schedule-next new-state))))
+      (schedule #(game-loop new-state)))))
 
 (defn start-game []
   (game-loop logic/initial-state))
