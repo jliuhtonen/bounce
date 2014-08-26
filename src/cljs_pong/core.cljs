13d49ab2b4e4a382dadb2867c4c94c263a7cc929
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2014-08-26 21:57:37 +0300

    Fix displaying score

--- src/cljs_pong/core.cljs
+++ src/cljs_pong/core.cljs
@@ -10,13 +10,16 @@
 
 (defn- schedule [f] (js/requestAnimationFrame f))
 
+(defn- update-screen! [state]
+  (draw/draw-game state)
+  (if (:score-changed state)
+    (let [scores (:score state)]
+      (ui/draw-player-1-score! scores)
+      (ui/draw-player-2-score! scores))))
+
 (defn- game-loop [state]
   (let [new-state (logic/update-state state (keyhandler/get-player-actions))]
-    (draw/draw-game new-state)
-    (if (:score-changed state)
-      (let [scores (:score state)]
-        (ui/draw-player-1-score! scores)
-        (ui/draw-player-2-score! scores)))
+    (update-screen! new-state)
     (if (:running new-state)
       (schedule #(game-loop new-state))
       (do
@@ -31,5 +34,5 @@
       (reset! game-in-progress true)
       (ui/count-down! 3 "GO" #(game-loop logic/initial-state)))))
 
-(draw/draw-game logic/initial-state)
+(update-screen! logic/initial-state)
 (keyhandler/register-key-handlers start-game)
